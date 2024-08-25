cbd7022ad2aef9b77327016310510ae0da714411
Andrea <andreja3219@gmail.com>
2024-08-25 01:56:49 -0500

    Diseño aplicacion con css y responsive design junto con la funcionalidad de encriptar un texto

---
+++ index.js
@@ -0,0 +1,28 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+    .replace(/e/gi, "xyl")
+    .replace(/i/gi, "kiwi")
+    .replace(/a/gi, "ze")
+    .replace(/o/gi, "octop")
+    .replace(/u/gi, "umbrel")
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./img/encriptado.jpg";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      alert("Ooops!", "Debes ingresar un texto", "warning");
+    }
+  }
+  
+  
+  
